c4663b4cad5148522962ffee521a0a17c9306fa0
naPOME <natipomebiz1234@gmail.com>
2024-01-31 02:40:17 +0300

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+    loop {
+    
+    println!("Please enter the number");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("your guess is :{}",guess);   
+    //generating random number using crate 
+   
+      
+    //Shadowing the guess var to change  its datatype
+    let guess:u32 = match guess.trim().parse(){
+        Ok(num)=>num,
+        Err(_)=>continue
+    };
+        
+    match guess.cmp(&secret_number){
+        Ordering::Less =>println!("too small"),
+        Ordering::Greater =>println!("too big"),
+        Ordering::Equal =>{
+            println!("you win");
+            break;}
+    }
+
+    }
 }
